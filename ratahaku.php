639ad62e6bae67cd4a6b46ca35d390d44a06a877
inffi <k1817@student.jamk.fi>
2016-04-04 14:06:14 +0300

    Ajax php still doesnt work

--- ratahaku.php
+++ ratahaku.php
@@ -3,10 +3,11 @@
 // tietokantayhteys
 include "connect.php";
 
-$haku = $_POST['haku'];
+$haku = $_GET['haku'];
 
 // haetaan ja tulostetaan HENKILO-taulun sisalto
-$haku_rata = "select RATA.rnimi,KAUPUNKI.nimi from RATA join KAUPUNKI on RATA.kaupunki_ID = KAUPUNKI.kaupunki_ID";
+// $haku_rata = "select RATA.rnimi,KAUPUNKI.nimi from RATA join KAUPUNKI on RATA.kaupunki_ID = KAUPUNKI.kaupunki_ID where KAUPUNKI.nimi like '%$haku%'";
+$haku_rata = "select * from KAUPUNKI where KAUPUNKI.nimi like '%$haku%'";
 $sql_rata = mysqli_query($connection,$haku_rata) or die("Can't find rata-data");
 
 //create an array
@@ -16,7 +17,7 @@ while($row =mysqli_fetch_assoc($slq_rata))
     $ratatiedot[] = $row;
 }
 
-echo json_encode($ratatiedot);
+print_r(json_encode($sql_rata));
 
 // while($row = mysqli_fetch_array($sql_rata)){
 //   echo "<div style='background-color: #CCC'>";
