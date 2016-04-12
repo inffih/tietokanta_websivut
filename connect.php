9b6325761d7b4328e024e9d7fddfc9367acefee9
inffi <k1817@student.jamk.fi>
2016-04-12 14:41:44 +0300

    charset to utf8

--- connect.php
+++ connect.php
@@ -9,6 +9,8 @@ $db = "K1719_1";
 // muodostetaan yhteys
 $connection = mysqli_connect($host,$user,$pass) or die("Connection to database failed");
 
+mysqli_set_charset($connection,"utf8");
+
 // valitaan tietokanta
 mysqli_select_db($connection,$db) or die("Can't select database");
 
